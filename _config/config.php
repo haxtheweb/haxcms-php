7e511d2d8166ba31f58290ebb38a652288c02932
btopro <btopro@outlook.com>
2018-07-25 16:11:50 -0400

    API key aspect documented in HAXCMS

--- _config/config.php
+++ _config/config.php
@@ -10,3 +10,14 @@ $HAXCMS->basePath = '/';
 // see system/lib/HAXCMS.php for additional deeper options
 // including $HAXCMS->user and $HAXCMS->password which can be used
 // to allow for lower permissioned users to login to specific sites
+
+// API keys - uncomment these in order to wire up more advanced API
+// functionality in HAX like youtube integration
+#$HAXCMS->apiKeys['youtube'] = '';
+#$HAXCMS->apiKeys['googlepoly'] = '';
+#$HAXCMS->apiKeys['memegenerator'] = '';
+#$HAXCMS->apiKeys['vimeo'] = '';
+#$HAXCMS->apiKeys['giphy'] = '';
+#$HAXCMS->apiKeys['unsplash'] = '';
+#$HAXCMS->apiKeys['flickr'] = '';
+#$HAXCMS->apiKeys['pixabay'] = '';
