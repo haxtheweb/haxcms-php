9faa6a69a1a6d5f75323e87b17b07d26a9009e35
btopro <btopro@outlook.com>
2018-08-11 23:49:19 -0400

    item updating and reflection into CMS as a whole

--- system/saveManifest.php
+++ system/saveManifest.php
@@ -6,11 +6,21 @@
     header('Content-Type: application/json');
     // load the site from name
     $site = $HAXCMS->loadSite($HAXCMS->safePost['siteName']);
-    // @todo update the items from the POST in the manifest
-    // leave the items array alone
-    $site->manifest->title = $HAXCMS->safePost['manifest']['title'];
+    // update these parts of the manifest to match POST
+    $site->manifest->title = filter_var($_POST['manifest']->title, FILTER_SANITIZE_STRING);
+    $site->manifest->description = filter_var($_POST['manifest']->description, FILTER_SANITIZE_STRING);
+    $site->manifest->metadata->icon = filter_var($_POST['manifest']->metadata->icon, FILTER_SANITIZE_STRING);
+    $site->manifest->metadata->theme = filter_var($_POST['manifest']->metadata->theme, FILTER_SANITIZE_STRING);
+    $site->manifest->metadata->image = filter_var($_POST['manifest']->metadata->image, FILTER_SANITIZE_STRING);
+    $site->manifest->metadata->hexCode = filter_var($_POST['manifest']->metadata->hexCode, FILTER_SANITIZE_STRING);
+    $site->manifest->metadata->cssVariable = filter_var($_POST['manifest']->metadata->cssVariable, FILTER_SANITIZE_STRING);
     $site->manifest->metadata->updated = time();
     $site->manifest->save();
+    // now work on HAXCMS layer to match the saved / sanitized data
+    $item = $site->manifest;
+    // remove items list as we only need the item itself not the nesting
+    unset($item->items);
+    $HAXCMS->outlineSchema->updateItem($item, TRUE);
     header('Status: 200');
     print json_encode($site->manifest);
     exit;
