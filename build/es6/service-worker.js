a3e414f8d1253f7ff768dd46738904bad161942a
btopro <btopro@outlook.com>
2019-03-13 17:20:27 -0400

    #115 git add -Agit add -A

--- build/es6/service-worker.js
+++ build/es6/service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["dist/build-full.js","8fc0093de0d7d91a0a6d5a59592afb68"],["dist/build.html","a36f9470ac42d076d05b280730d27e2b"],["node_modules/web-animations-js/web-animations-next-lite.min.js","6579d2914a8920d46d8cc74a3cff3dec"]];
+var precacheConfig = [["dist/build-full.js","93740aab1614cb2db86ebff243e103a8"],["dist/build.html","40349843d029366ae2e5c432ae88e98a"],["node_modules/web-animations-js/web-animations-next-lite.min.js","6579d2914a8920d46d8cc74a3cff3dec"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
