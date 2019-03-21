ca88bc351d04685cc73f0406c47d68e4575192ee
btopro <btopro@outlook.com>
2019-03-21 15:21:08 -0400

    save page can not handle fields!

--- system/savePage.php
+++ system/savePage.php
@@ -41,22 +41,70 @@
         foreach ($details as $key => $value) {
           // sanitize both sides
           $key = filter_var($key, FILTER_SANITIZE_STRING);
-          $value = filter_var($value, FILTER_SANITIZE_STRING);
-          if ($key == 'location') {
-            // check on name
-            $cleanTitle = $HAXCMS->cleanTitle($value);
-            // ensure this isn't just saying to keep the same name
-            if ($cleanTitle != str_replace('pages/', '', str_replace('/index.html', '', $page->location))) {
-              $tmpTitle = $site->getUniqueLocationName($cleanTitle);
-              $location = 'pages/' . $tmpTitle . '/index.html';
-              // move the folder
-              $site->renamePageLocation($page->location, $location);
-              $page->location = $location;
-            }
-          }
-          // don't allow key to be changed
-          else if ($key != 'id') {
-            $page->{$key} = $value;
+          switch ($key) {
+            case 'location':
+              // check on name
+              $value = filter_var($value, FILTER_SANITIZE_STRING);
+              $cleanTitle = $HAXCMS->cleanTitle($value);
+              // ensure this isn't just saying to keep the same name
+              if ($cleanTitle != str_replace('pages/', '', str_replace('/index.html', '', $page->location))) {
+                $tmpTitle = $site->getUniqueLocationName($cleanTitle);
+                $location = 'pages/' . $tmpTitle . '/index.html';
+                // move the folder
+                $site->renamePageLocation($page->location, $location);
+                $page->location = $location;
+              }
+            break;
+            case 'title':
+            case 'description':
+              $value = filter_var($value, FILTER_SANITIZE_STRING);
+              $page->{$key} = $value;
+            break;
+            case 'created':
+              $value = filter_var($value, FILTER_VALIDATE_INT);
+              $page->metadata->created = $value;
+            break;
+            case 'theme':
+              $themes = $GLOBALS['HAXCMS']->getThemes();
+              $value = filter_var($value, FILTER_SANITIZE_STRING);
+              if (isset($themes->{$value})) {
+                $page->metadata->theme = $themes->{$value};
+                $page->metadata->theme->key = $value;
+              }
+              break;
+            default:
+              // ensure ID is never changed
+              if ($key != 'id') {
+                // support for saving fields
+                if (!isset($page->metadata->fields)) {
+                  $page->metadata->fields = new stdClass();
+                }
+                switch (gettype($value)) {
+                  case 'array':
+                  case 'object':
+                    $page->metadata->fields->{$key} = new stdClass();
+                    foreach ($value as $key2 => $val) {
+                      $page->metadata->fields->{$key}->{$key2} = new stdClass();
+                      $key2 = filter_var($key2, FILTER_VALIDATE_INT);
+                      foreach ($val as $key3 => $deepVal) {
+                        $key3 = filter_var($key3, FILTER_SANITIZE_STRING);
+                        $deepVal = filter_var($deepVal, FILTER_SANITIZE_STRING);
+                        $page->metadata->fields->{$key}->{$key2}->{$key3} = $deepVal;
+                      }
+                    }
+                  break;
+                  case 'integer':
+                  case 'double':
+                    $value = filter_var($value, FILTER_VALIDATE_INT);
+                    $page->metadata->fields->{$key} = $value;
+                  break;
+                  default:
+                    $value = filter_var($value, FILTER_SANITIZE_STRING);
+                    $page->metadata->fields->{$key} = $value;
+                  break;
+                }
+              }
+            break;
           }
         }
         $site->updatePage($page);
