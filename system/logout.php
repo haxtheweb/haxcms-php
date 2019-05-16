3981c9b7c4cfcfc92717dff8df0935d4cba41f69
btopro <btopro@outlook.com>
2019-05-16 11:45:38 -0400

    allow logout to be influence by configuration

--- system/logout.php
+++ system/logout.php
@@ -1,5 +1,7 @@
 <?php
 // @todo need to run some kind of shut down routine for logging out
+include_once '../system/lib/bootstrapHAX.php';
+include_once $HAXCMS->configDirectory . '/config.php';
 header('Content-Type: application/json');
 header('Status: 200');
 print json_encode('loggedout');
