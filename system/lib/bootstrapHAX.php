a8c8ae995bbd27d89a08871612f0642f1b0809f2
btopro <btopro@outlook.com>
2019-05-17 08:00:52 -0400

    support for IAM configuration in core

--- system/lib/bootstrapHAX.php
+++ system/lib/bootstrapHAX.php
@@ -13,9 +13,22 @@ global $config;
 // support for config.php to override core capabilities
 $config['connection'] = array();
 // calculate where we are in the file system, accurately
-$here = __FILE__;
-define('HAXCMS_ROOT', str_replace('/system/lib/bootstrapHAX.php', '', $here));
+$here = str_replace('/system/lib/bootstrapHAX.php', '', __FILE__);
+// core support for IAM symlinked core which follows a similar pattern at a custom base path
+if (file_exists($here . '/IAM')) {
+  $pieces = explode('/', $_SERVER['REQUEST_URI']);
+  array_shift($pieces);
+  $here = str_replace('cores/HAXcms', 'users/' . $pieces[0], $here);
+}
+define('HAXCMS_ROOT', $here);
 // the whole CMS as one object
 include_once 'HAXCMS.php';
 // invoke the CMS
 $HAXCMS = new HAXCMS();
+// support IAM config
+if (file_exists($here . '/IAM')) {
+    $HAXCMS->config->iam = true;
+    if (file_exists($here . '/../../_iamConfig/config.php')) {
+        include_once $here . '/../../_iamConfig/config.php';
+    }
+}
