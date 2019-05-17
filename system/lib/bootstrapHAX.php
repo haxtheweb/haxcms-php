918c3aae1d29e7d58b687e798f6bb39732de65df
btopro <btopro@outlook.com>
2019-05-17 16:13:02 -0400

    bootstrap support HAXiam correctly

--- system/lib/bootstrapHAX.php
+++ system/lib/bootstrapHAX.php
@@ -15,18 +15,22 @@ $config['connection'] = array();
 // calculate where we are in the file system, accurately
 $here = str_replace('/system/lib/bootstrapHAX.php', '', __FILE__);
 // core support for IAM symlinked core which follows a similar pattern at a custom base path
-if (file_exists($here . '/IAM')) {
+// this is needed because of how PHP resolves paths when in symlinked patterns
+if (file_exists($here . '/_config/IAM')) {
   $pieces = explode('/', $_SERVER['REQUEST_URI']);
   array_shift($pieces);
-  $here = str_replace('cores/HAXcms', 'users/' . $pieces[0], $here);
+  // leverage BRANCH in order to calculate the correct directory name here
+  if ($branch = file_get_contents($here . '/BRANCH.txt')) {
+    $here = str_replace('cores/HAXcms-' . $branch, 'users/' . $pieces[0], $here);
+  }
 }
 define('HAXCMS_ROOT', $here);
 // the whole CMS as one object
 include_once 'HAXCMS.php';
 // invoke the CMS
 $HAXCMS = new HAXCMS();
-// support IAM config
-if (file_exists($here . '/IAM')) {
+// support IAM config now apply these changes
+if (file_exists($here . '/_config/IAM')) {
     $HAXCMS->config->iam = true;
     if (file_exists($here . '/../../_iamConfig/HAXcmsConfig.php')) {
         include_once $here . '/../../_iamConfig/HAXcmsConfig.php';
