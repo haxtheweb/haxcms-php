bfdeeeba4f26e8343411f688ec9e7878a10da5a0
btopro <btopro@outlook.com>
2020-03-18 11:41:32 -0400

    cdn fix

--- system/boilerplate/site/build-haxcms.js
+++ system/boilerplate/site/build-haxcms.js
@@ -2,7 +2,7 @@
 // this ties in custom theme files as well as removes DOM nodes related
 // to serving a legacy audience in the event this is evergreen (most times)
 var def = document.getElementsByTagName('script')[0];
-var cdn = "./";
+ var cdn = "./";
 if (window.__appCDN) {
   cdn = window.__appCDN;
 }
