98f0d5f89e2943b4affd962a837b618700a2f38d
btopro <btopro@outlook.com>
2020-03-27 14:54:40 -0400

    https://github.com/elmsln/issues/issues/150

--- system/boilerplate/site/index.php
+++ system/boilerplate/site/index.php
@@ -3,6 +3,7 @@
   include_once $HAXCMS->configDirectory . '/config.php';
   $site = $HAXCMS->loadSite(basename(__DIR__));
   $page = $site->loadNodeByLocation();
+  $color = 'var(' . $site->manifest->metadata->theme->variables->cssVariable . ', #FF2222)';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,43 +14,122 @@
   <style>
     body {
       margin: 0;
-      min-height: 100vh;
+      min-height: 98vh;
     }
+  </style>
+  <style id="loadingstyles">
     haxcms-site-builder {
       display: block;
-    }
-    haxcms-site-builder:not(:defined) div.loading {
-      width: 100vw;
-      display: block;
-      position: initial;
-      height: 100vh;
-      background: orange;
-    }
-    haxcms-site-builder:not(:defined) div {
-      font-size: 6vw;
-      line-height: 1;
-      margin: 0 auto;
-      top: calc(50vh - 8vw);
-      width: 100%;
-      justify-content: center;
-      display: grid;
-      text-align: center;
-      padding: 0;
-      position: relative;
-      font-family: "Courier New", Courier, monospace;
-      color: black;
     }
     body[no-js] haxcms-site-builder {
       display: none !important;
     }
-  </style>
+    #loading {
+      background-color: white;
+      bottom: 0px;
+      left: 0px;
+      opacity: 1;
+      position: absolute;
+      right: 0px;
+      top: 0px;
+      transition: all linear 300ms;
+      -webkit-transition: all linear 300ms;
+      z-index: 99999999;
+    }
+
+    #loading.loaded {
+      animation: fade-out .7s ease-in-out;
+      animation-fill-mode: forwards;
+    }
+    #loading div.messaging {
+      color: rgba(255,255,255, 0.7);
+      font-family: Roboto;
+      left: 0px;
+      margin-top: -75px;
+      position: absolute;
+      right: 0px;
+      text-align: center;
+      top: 50%;
+      transform: translateY(-50%);
+    }
+    #loading div.messaging h1 {
+      color: white;
+      font-family: 'Open Sans', 'arial', 'serif';
+      font-size: 40px;
+      background-color: <?php print $color; ?>;
+      transition: .4s ease-in-out all;
+    }
+    #loading img {
+      transition: .4s all ease-in-out;
+      width: 300px;
+      height: 300px;
+      margin-bottom: 50px;
+      border-radius: 50%;
+      border: 8px solid <?php print $color;?>;
+      box-shadow: 0 4px 12px 0 rgba(0, 0, 0, 0.45);
+      -moz-box-shadow: 0 4px 12px 0 rgba(0, 0, 0, 0.45);
+	    -webkit-box-shadow: 0 4px 12px 0 rgba(0, 0, 0, 0.45);
+      -ms-filter: "progid:DXImageTransform.Microsoft.Shadow(Strength=4, Direction=135, Color='#444444')";
+    }
+    .progress-line,
+    .progress-line:before {
+      height: 8px;
+      width: 100%;
+      margin: auto;
+    }
+    .progress-line {
+      background-color: rgba(0,0,0, 0.05);
+      display: -webkit-flex;
+      display: flex;
+      width: 300px;
+    }
+    .progress-line:before {
+      background-color: <?php print $color;?>;
+      content: '';
+      animation: running-progress 2s cubic-bezier(0.4, 0, 0.2, 1) infinite;
+    }
+    @keyframes running-progress {
+      0% {
+        margin-left: 0px;
+        margin-right: 100%;
+      }
+      50% {
+        margin-left: 25%;
+        margin-right: 0%;
+      }
+      100% {
+        margin-left: 100%;
+        margin-right: 0;
+      }
+    }
+    @keyframes fade-out {
+      0% {
+        opacity: 1;
+      }
+      99% {
+        opacity: 0;
+      }
+      100% {
+        opacity: 0;
+      }
+    }
+    </style>
+    <script id="loadingscript">
+    window.addEventListener('load', function(e) {
+      // give the web components a second to build
+      setTimeout(function() {
+        document.querySelector('#loading').classList.add('loaded');
+        setTimeout(function() {
+          document.querySelector('#loading').parentNode.removeChild(document.querySelector('#loading'));
+          document.querySelector('#loadingstyles').parentNode.removeChild(document.querySelector('#loadingstyles'));
+          document.querySelector('#loadingscript').parentNode.removeChild(document.querySelector('#loadingscript'));
+        }, 600);
+      }, 1000)
+    });
+  </script>
 </head>
 <body no-js <?php print $site->getSitePageAttributes();?>>
   <haxcms-site-builder id="site" file="site.json<?php print $HAXCMS->cacheBusterHash();?>">
-    <div class="loading">
-      <div><?php print $site->name; ?></div>
-      <div>loading</div>
-    </div>
     <?php print $site->getPageContent($page); ?>
   </haxcms-site-builder>
   <div id="haxcmsoutdatedfallback">
@@ -60,6 +140,13 @@
       <iframe id="content" style="width:80%;float:left;height:500px;padding:0;margin:0;" name="content" id="frame2" src=""></iframe>
       <div style="float:left;padding:16px 0;font-size:32px;text-align: center;width:100%;">Please use a modern browser to
         view our website correctly. <a href="http://outdatedbrowser.com/">Update my browser now</a></div>
+    </div>
+  </div>
+  <div id="loading">
+    <div class="messaging">
+      <img src="<?php print $site->manifest->metadata->site->logo;?>" alt="" />
+      <div class="progress-line"></div>
+      <h1>Loading <?php print $site->name; ?>..</h1>
     </div>
   </div>
   <script>document.body.removeAttribute('no-js');window.__appCDN="<?php print $HAXCMS->getCDNForDynamic();?>";window.__appForceUpgrade=<?php print $site->getForceUpgrade();?>;</script>
