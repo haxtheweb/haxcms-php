ceb98ce82068c08200f629b0c5d208d1db6169e4
heymp <mgp140@gmail.com>
2020-04-06 14:01:41 -0400

    added wc-autoloader gulp task https://github.com/elmsln/issues/issues/159

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
-const gulp = require('gulp');
-const terser = require('gulp-terser');
+const gulp = require("gulp");
+const terser = require("gulp-terser");
+const glob = require("glob");
+const fs = require("fs");
+const path = require("path");
 gulp.task(
-  "default", () => {
+  "terser", () => {
     // now work on all the other files
     gulp.src('./build/es6/**/*.js')
     .pipe(terser({
@@ -22,3 +25,39 @@ gulp.task(
       .pipe(gulp.dest('./build/es6-amd/'));
   }
 );
+
+gulp.task("wc-autoloader", async () => {
+  glob(path.join("./build/es6/**/*.js"), (er, files) => {
+    let elements = [];
+    // async loop over files
+    files.forEach((file) => {
+      // first check if this is a file we need to get.
+      if (file.includes("node_modules")) {
+        // grab the name of the file
+        const fName = file.replace("node_modules/", "");
+
+        if (fs.existsSync(file)) {
+          const contents = fs.readFileSync(file, "utf8");
+          // This Regex is looking for tags that are defined by string values
+          // this will work for customElements.define("local-time",s))
+          // This will NOT work for customElements.define(LocalTime.tagName,s))
+          const defineStatements = /customElements\.define\(["|'|`](.*?)["|'|`]/gm.exec(
+            contents
+          );
+          if (defineStatements) {
+            const tagName = defineStatements[1];
+            elements.push({ [tagName]: fName });
+          }
+        }
+      }
+    });
+
+    // write entries to file
+    // use null, 2 to format JSON
+    fs.writeFileSync(
+      path.join(__dirname, "wc-autoloader.json"),
+      JSON.stringify(elements, null, 4),
+      "utf8"
+    );
+  });
+});
