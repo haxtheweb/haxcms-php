8bd0bc422aef2f43a55a79b5498f7d1c26b1b1f2
btopro <btopro@outlook.com>
2020-04-10 14:01:40 -0400

    build up

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ const path = require("path");
 gulp.task(
   "terser", () => {
     // now work on all the other files
-    gulp.src('./build/es6/**/*.js')
+    return gulp.src('./build/es6/**/*.js')
       .pipe(terser({
         ecma: 2017,
         keep_fnames: true,
@@ -14,15 +14,6 @@ gulp.task(
         module: true,
       }))
       .pipe(gulp.dest('./build/es6/'));
-    // now work on all the other files
-    return gulp.src('./build/es6-amd/**/*.js')
-      .pipe(terser({
-        keep_fnames: true,
-        mangle: true,
-        module: false,
-        safari10: true,
-      }))
-      .pipe(gulp.dest('./build/es6-amd/'));
   }
 );
 
