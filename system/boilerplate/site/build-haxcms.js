54b17efb3c6713573407bce01ce545b430422e28
btopro <btopro@outlook.com>
2020-05-14 11:39:01 -0400

    https://github.com/elmsln/issues/issues/217

--- system/boilerplate/site/build-haxcms.js
+++ system/boilerplate/site/build-haxcms.js
@@ -1,42 +1,51 @@
 // HAXcms specific clean up and platforn integration
 // this ties in custom theme files as well as removes DOM nodes related
 // to serving a legacy audience in the event this is evergreen (most times)
-try {
-  var def = document.getElementsByTagName('script')[0];
-  // if a dynamic import fails, we bail over to the compiled version
-  new Function('import("");');
-  // remove fallback cause if we passed dynamic import then we are evergreen
-  if (document.getElementById("haxcmsoutdatedfallback")) {
-    document.body.removeChild(document.getElementById("haxcmsoutdatedfallback"));
-  }
-  var build2 = document.createElement('script');
-  build2.src = './custom/build/custom.es6.js';
-  build2.type = 'module';
-  def.parentNode.insertBefore(build2, def);
-} catch (err) {
-  var ancient=false;
+if (/^h/.test(document.location)) {
   try {
-    if (typeof Symbol == "undefined") { // IE 11, at least try to serve a watered down site
-      ancient = true;
-    }
-    new Function('let a;'); // bizarre but needed for Safari 9 bc of when it was made
-  }
-  catch (err) {
-    ancient = true;
-  }
-  if (!ancient) {
+    var def = document.getElementsByTagName('script')[0];
+    // if a dynamic import fails, we bail over to the compiled version
+    new Function('import("");');
     // remove fallback cause if we passed dynamic import then we are evergreen
     if (document.getElementById("haxcmsoutdatedfallback")) {
       document.body.removeChild(document.getElementById("haxcmsoutdatedfallback"));
     }
-  }
-  else {
-    // we bombed somewhere above, this implies that it's some odd between version
-    // most likely Safari 9ish, IE pre 11 and anything uber old. Serve no JS variation
-    if (document.getElementById('site')) {
-      document.getElementById('site').style.display = 'none';
-      document.getElementById('haxcmsoutdatedfallbacksuperold').style.display = 'block';
+    var build2 = document.createElement('script');
+    build2.src = './custom/build/custom.es6.js';
+    build2.type = 'module';
+    def.parentNode.insertBefore(build2, def);
+  } catch (err) {
+    var ancient=false;
+    try {
+      if (typeof Symbol == "undefined") { // IE 11, at least try to serve a watered down site
+        ancient = true;
+      }
+      new Function('let a;'); // bizarre but needed for Safari 9 bc of when it was made
     }
+    catch (err) {
+      ancient = true;
+    }
+    if (!ancient) {
+      // remove fallback cause if we passed dynamic import then we are evergreen
+      if (document.getElementById("haxcmsoutdatedfallback")) {
+        document.body.removeChild(document.getElementById("haxcmsoutdatedfallback"));
+      }
+    }
+    else {
+      // we bombed somewhere above, this implies that it's some odd between version
+      // most likely Safari 9ish, IE pre 11 and anything uber old. Serve no JS variation
+      if (document.getElementById('site')) {
+        document.getElementById('site').style.display = 'none';
+        document.getElementById('haxcmsoutdatedfallbacksuperold').style.display = 'block';
+      }
+    }
+  }
+} else {
+  // this implies we are offline, viewing the files locally
+  // so let's show the simplistic site viewer / iframe theme
+  if (document.getElementById('site')) {
+    document.getElementById('site').style.display = 'none';
+    document.getElementById('haxcmsoutdatedfallbacksuperold').style.display = 'block';
   }
 }
 var cdn = "./";
