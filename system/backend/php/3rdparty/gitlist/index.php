4514cbaabcedb01644d4fb99a407cc42028c41d1
btopro <btopro@outlook.com>
2021-01-08 16:20:59 -0500

    support for iam in gitlist

--- system/backend/php/3rdparty/gitlist/index.php
+++ system/backend/php/3rdparty/gitlist/index.php
@@ -20,6 +20,14 @@ if (!is_writable(__DIR__ . DIRECTORY_SEPARATOR . 'cache')) {
 require 'vendor/autoload.php';
 
 $config = GitList\Config::fromFile('config.ini');
+// support for IAM based environment if we detect the file that indicates the HAXcms is in a iam deploy
+if (file_exists(str_replace('/system/backend/php/3rdparty/gitlist', '/_config/IAM', __DIR__))) {
+    $tmp = explode('/', $_SERVER['REQUEST_URI']);
+    array_shift($tmp);
+    $iamPath = array_shift($tmp);
+    $config->set('git', 'repositories', array(0 => "/var/www/iam/users_sites/" . $iamPath . "/sites/"));
+}
+  
 if ($config->get('date', 'timezone')) {
     date_default_timezone_set($config->get('date', 'timezone'));
 }
