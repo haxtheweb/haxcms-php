1e22b6aa9e690875ea68dcc0d07849019472a0a7
btopro <btopro@outlook.com>
2023-04-28 14:56:40 -0400

    https://github.com/elmsln/issues/issues/1339

--- system/backend/php/lib/Operations.php
+++ system/backend/php/lib/Operations.php
@@ -3096,6 +3096,7 @@ class Operations {
   public function cloneSite() {
     $site = $GLOBALS['HAXCMS']->loadSite($this->params['site']['name']);
     $siteDirectoryPath = $site->directory . '/' . $site->manifest->metadata->site->name;
+    $originalPathForReplacement = "/sites/" . $site->manifest->metadata->site->name . "/files/";
     $cloneName = $GLOBALS['HAXCMS']->getUniqueName($site->name);
     // ensure the path to the new folder is valid
     $GLOBALS['fileSystem']->mirror(
@@ -3106,6 +3107,23 @@ class Operations {
     $site = $GLOBALS['HAXCMS']->loadSite($cloneName);
     $site->manifest->metadata->site->name = $cloneName;
     $site->manifest->id = $GLOBALS['HAXCMS']->generateUUID();
+    // loop through all items and rewrite the path to files as we cloned it
+    foreach ($site->manifest->items as $delta => $item) {
+      if (isset($item->metadata->files)) {
+        foreach ($item->metadata->files as $delta2 => $file) {
+          $site->manifest->items[$delta]->metadata->files[$delta2]->path = str_replace(
+            $originalPathForReplacement,
+            '/sites/' . $cloneName . '/files/',
+            $site->manifest->items[$delta]->metadata->files[$delta2]->path
+          );
+          $site->manifest->items[$delta]->metadata->files[$delta2]->fullUrl = str_replace(
+            $originalPathForReplacement,
+            '/sites/' . $cloneName . '/files/',
+            $site->manifest->items[$delta]->metadata->files[$delta2]->fullUrl
+          );
+        }
+      }
+    }
     $site->save();
     return array(
       'status' => 200,
