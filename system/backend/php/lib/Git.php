5767b42dd0df35869659cec3748f7c4dc98170ca
btopro <bto108@psu.edu>
2026-07-30 15:17:18 -0400

    https://github.com/haxtheweb/issues/issues/2948

--- system/backend/php/lib/Git.php
+++ system/backend/php/lib/Git.php
@@ -11,6 +11,32 @@
  * @copyright  Copyright 2013 James Brumond
  * @repo       http://github.com/kbjr/Git.php
  * @fork       ELMS:LN team
+ * @license    MIT (elected from the upstream dual MIT / GPL-3.0 grant)
+ *
+ * Upstream license text: https://github.com/kbjr/Git.php/blob/master/license.txt
+ * This distribution elects the MIT branch of the dual license. The MIT
+ * copyright and permission notice below satisfy MIT's attribution
+ * requirement in-file; the GPL-3.0 branch is not exercised here.
+ *
+ * MIT LICENSE
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 
 if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) {
